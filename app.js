1b94858f3ff849b3dfd4e4e4f890f84154fe49d6
A. Alejandra Ruiz Sánchez <amaralexrusan@gmail.com>
2024-07-22 02:20:26 -0600

    Versión final Juego del Número Secreto

---
+++ app.js
@@ -0,0 +1,12 @@
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+
+}
+
+function intentoUsuario() {
+    alert('Click desde el botón');
+}
+
+asignarTextoElemento('h1', '¡Juego del número secreto!');
+asignarTextoElemento('p', 'Indica un número del 1 al 10');
